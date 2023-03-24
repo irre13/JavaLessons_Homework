859beda2eacb1d3e314350f79b8f4b98bdfe7503
Vladimir Stepankin <vladimirstepankin@mail.ru>
2023-03-24 14:38:38 +0300

    first commit

---
+++ src/main/java/lesson1/HomeworkLessonOne.java
@@ -0,0 +1,20 @@
+package lesson1;
+
+public class HomeworkLessonOne {
+    public static void main(String[] args) {
+        exerciseOne();
+        exerciseTwo();
+        exerciseThree();
+    }
+
+    private static void exerciseOne() {
+    }
+
+    private static void exerciseTwo() {
+
+    }
+
+    private static void exerciseThree() {
+
+    }
+}
